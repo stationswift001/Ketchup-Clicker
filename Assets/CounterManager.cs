c736750fc97dafbd8e8217b130570d867670b3f7
stationswift001 <36418561+stationswift001@users.noreply.github.com>
2021-11-29 21:53:58 -0600

    Initial commit

---
+++ Assets/CounterManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CounterManager : MonoBehaviour
+{
+    string pre;
+    double ctrvalue;
+    string post;
+    void format()
+    {
+        
+    }
+    //Add to counter 
+    void add(double amnt) 
+    {
+
+    }
+}
